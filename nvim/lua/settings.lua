bae165ced1204ad6f288ffe7b95d42ad551f6eda
David Kariukí <davidkariuki@users.noreply.github.com>
2021-04-03 12:15:41 +0100

    Clean up keybinds

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,55 +1,48 @@
-local utils = require("utils")
-local cmd = vim.cmd
 local indent = 2
 
-cmd "syntax enable"
-cmd "filetype plugin indent on"
+vim.cmd "syntax enable"
+vim.cmd "filetype plugin indent on"
 
-utils.opt("b", "shiftwidth", indent)
-utils.opt("b", "autoindent", true)
-utils.opt("b", "smartindent", true)
-utils.opt("b", "expandtab", true)
-utils.opt("b", "tabstop", indent)
-utils.opt("b", "softtabstop", indent)
-utils.opt("o", "hidden", true)
-utils.opt("o", "ruler", true)
-utils.opt("o", "ignorecase", true)
-utils.opt("o", "smartcase", true)
-utils.opt("o", "scrolloff", 4 )
-utils.opt("o", "shiftround", true)
-utils.opt("o", "splitbelow", true)
-utils.opt("o", "splitright", true)
-utils.opt("o", "wildmode", "list:longest")
-utils.opt("o", "pastetoggle", "<Leader>p")
-utils.opt("o", "ch", 1)
-utils.opt("w", "number", true)
-utils.opt("w", "cursorline", true)
-utils.opt("w", "relativenumber", true)
-utils.opt("w", "wrap", false)
-utils.opt("w", "signcolumn", "yes")
+vim.bo.shiftwidth = indent
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.bo.expandtab = true
+vim.bo.tabstop = indent
+vim.bo.softtabstop = indent
+vim.o.hidden = true
+vim.o.ruler = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.scrolloff = 4
+vim.o.shiftround = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.wildmode = "list:longest"
+vim.o.pastetoggle = "<Leader>p"
+vim.o.ch = 1
+vim.wo.number = true
+vim.wo.cursorline = true
+vim.wo.relativenumber = true
+vim.wo.wrap = false
+vim.wo.signcolumn = "yes"
 
 -- Highlight on yank
-cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}"
-
--- colorscheme
-utils.opt("o", "termguicolors", true)
-utils.opt("o", "background", "dark")
-cmd "colorscheme one"
-
--- completion
-utils.opt("o", "completeopt", "menu,menuone,noinsert,noselect")
-cmd [[set shortmess+=c]]
-vim.g.completion_confirm_key = ""
-vim.g.completion_matching_strategy_list = {"exact", "substring", "fuzzy"}
-vim.g.diagnostic_enable_underline = 1
-
-
--- <Tab> to navigate the completion menu
-utils.map("i", "<S-Tab>", "pumvisible() ? '\\<C-p>' : '\\<Tab>'", {expr = true})
-utils.map("i", "<Tab>", "pumvisible() ? '\\<C-n>' : '\\<Tab>'", {expr = true})
+vim.cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}"
 
 -- Formatoptions
-cmd "au BufEnter * set fo-=c fo-=r fo-=o"
+vim.cmd "au BufEnter * set fo-=c fo-=r fo-=o"
+
+-- colorscheme
+vim.o.termguicolors = true
+vim.o.background = "dark"
+vim.cmd "colorscheme one"
+
+-- completion
+vim.o.completeopt =  "menu,menuone,noinsert,noselect"
+vim.cmd "set shortmess+=c"
+vim.g.completion_confirm_key = ""
+vim.g.completion_matching_strategy_list = { "exact", "substring", "fuzzy" }
+vim.g.diagnostic_enable_underline = 1
 
 -- LSP diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
