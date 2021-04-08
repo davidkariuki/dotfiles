5cc9eb6b2e83593b75a800bc460b5c69a904df62
David Kariukí <davidkariuki@users.noreply.github.com>
2021-04-08 18:59:31 +0100

    Format async, don't care about :wq

--- nvim/lua/config/lsp/lspinstall.lua
+++ nvim/lua/config/lsp/lspinstall.lua
@@ -20,7 +20,7 @@ local on_attach = function(client, bufnr)
   if client.resolved_capabilities.document_formatting then
     vim.api.nvim_command [[augroup Format]]
     vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)]]
+    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()]]
     vim.api.nvim_command [[augroup END]]
   end
 end
