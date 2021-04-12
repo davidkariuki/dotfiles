f519db581a106dcb82fbd71d3cc527866d2da709
David Kariukí <davidkariuki@users.noreply.github.com>
2021-04-12 09:14:26 +0100

    Telescope config

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -2,8 +2,22 @@
 
 require("telescope").setup {
   defaults = {
+    prompt_position = "top",
+    prompt_prefix = " 🔎 ",
     generic_sorter =  require"telescope.sorters".get_fzy_sorter,
     file_sorter =  require"telescope.sorters".get_fzy_sorter,
+    layout_strategy = "flex",
+    shorten_path = true,
+    use_less = true,
+    layout_defaults = {
+      horizontal = {
+        mirror = false,
+        preview_width = 0.6
+      },
+      vertical = {
+        mirrot = false
+      }
+    }
   }
 }
 
