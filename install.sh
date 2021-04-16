b7ac06a1d84ac84e0e4d1f81b1b385aaaddefd78
David Kariukí <davidkariuki@users.noreply.github.com>
2021-04-16 15:13:55 +0100

    Change tmux install script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+cp -rsf "$(pwd)/nvim/" ~/.config/
 ln -fs "$(pwd)/shell/.gitconfig" ~/.gitconfig
 ln -fs "$(pwd)/shell/.zshrc" ~/.zshrc
 ln -fs "$(pwd)/shell/.p10k.zsh" ~/.p10k.zsh
 ln -fs "$(pwd)/shell/.tmux.conf" ~/.tmux.conf
 ln -fs "$(pwd)/shell/kitty.conf" ~/.config/kitty/kitty.conf
-ln -fs "$(pwd)/shell/tmuxinator/dotfiles.yml" ~/.config/tmuxinator/dotfiles.yml
-cp -rsf "$(pwd)/nvim/" ~/.config/
+
+for file in "$(pwd)"/shell/tmuxinator/*.yml; do 
+  filename=$(basename "$file")
+  ln -fs "$file" ~/.config/tmuxinator/"$filename"
+done
