cb1bbe367ccaf13487887a58ff4b921a0635e1c1
David Kariukí <davidkariuki@users.noreply.github.com>
2021-04-29 09:05:58 +0100

    Add svelte to emmet autocomplete

--- nvim/lua/config/emmet.lua
+++ nvim/lua/config/emmet.lua
@@ -1,1 +1,1 @@
-vim.cmd "au FileType html,jsx,tsx,css imap <expr> <tab> emmet#expandAbbrIntelligent('\\<tab>')"
+vim.cmd "au FileType html,jsx,tsx,css,svelte imap <expr> <tab> emmet#expandAbbrIntelligent('\\<tab>')"
