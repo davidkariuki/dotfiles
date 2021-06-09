5bd4cbe9bb67c5fecd4e4cb77805e4631c71bf93
David Kariukí <davidkariuki@users.noreply.github.com>
2021-06-09 04:13:59 +0100

    Add js/ts-react filetypes to emmet expansion

--- nvim/lua/config/emmet.lua
+++ nvim/lua/config/emmet.lua
@@ -1,1 +1,1 @@
-vim.cmd "au FileType html,jsx,tsx,css,svelte imap <expr> <tab> emmet#expandAbbrIntelligent('\\<tab>')"
+vim.cmd "au FileType html,jsx,tsx,css,svelte,javascriptreact,typescriptreact imap <expr> <tab> emmet#expandAbbrIntelligent('\\<tab>')"
