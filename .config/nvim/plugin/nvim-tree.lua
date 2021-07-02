c42dd61e8836f400389895d00b5f72f7a8b8c62c
David Kariukí <davidkariuki@users.noreply.github.com>
2021-07-02 07:02:13 +0100

    Update nvim-tree config

--- .config/nvim/plugin/nvim-tree.lua
+++ .config/nvim/plugin/nvim-tree.lua
@@ -1,16 +1,17 @@
 local tree_cb = require"nvim-tree.config".nvim_tree_callback
 
 vim.g.nvim_tree_bindings = {
-  ["n"] = tree_cb("create"),
-  ["v"] = tree_cb("vsplit"),
-  ["s"] = tree_cb("split"),
-  ["t"] = tree_cb("tabnew"),
+  { key = "n", cb = tree_cb("create") },
+  { key = "v", cb = tree_cb("vsplit") },
+  { key = "s", cb = tree_cb("split") },
+  { key = "t", cb = tree_cb("tabnew") },
+  { key = { "<CR>", "o" }, cb = tree_cb("edit") },
 }
 
 vim.g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
 vim.g.nvim_tree_auto_open = 0
 vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_tab_open = 1
+vim.g.nvim_tree_tab_open = 0
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_show_icons = {
   git = 1,
