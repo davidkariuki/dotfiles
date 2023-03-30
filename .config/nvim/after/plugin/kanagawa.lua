d740d65de29f26deb3372d17b43a48e5ad6bb7b6
David Kariukí <davidkariuki@users.noreply.github.com>
2023-03-30 07:42:14 +0100

    Telescope overrides

--- .config/nvim/after/plugin/kanagawa.lua
+++ .config/nvim/after/plugin/kanagawa.lua
@@ -5,14 +5,13 @@ require("kanagawa").setup({
     return {
       NormalFloat = { bg = "none" },
       FloatBorder = { bg = "none" },
-      -- Save an hlgroup with dark background and dimmed foreground
-      -- so that you can use it where your still want darker windows.
-      -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-      -- NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-      -- Popular plugins that open floats will link to NormalFloat by default;
-      -- set their background accordingly if you wish to keep them dark and borderless
-      -- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-      -- MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+      TelescopeTitle = { fg = theme.ui.special, bold = true },
+      TelescopePromptNormal = { bg = "none" },
+      TelescopePromptBorder = { fg = "none", bg = "none" },
+      TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = "none" },
+      TelescopeResultsBorder = { fg = "none", bg = "none" },
+      TelescopePreviewNormal = { bg = "none" },
+      TelescopePreviewBorder = { bg = "none" },
     }
   end,
   colors = {
