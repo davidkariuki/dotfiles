206818c1d78018654abbce38622d7301a8e7fb94
David Kariukí <davidkariuki@users.noreply.github.com>
2023-10-13 09:55:58 +0100

    Add leetcode

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -125,6 +125,40 @@ return require("lazy").setup({
 
   -- Terminal
   "voldikss/vim-floaterm",
+
+  -- Leetcode
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      domain = "com",
+      arg = "leetcode.nvim",
+      lang = "ruby",
+      sql = "mysql",
+      directory = vim.fn.stdpath("data") .. "/leetcode/",
+      logging = true,
+
+      console = {
+        open_on_runcode = true,
+
+        size = {
+          width = "75%",
+          height = "75%",
+        },
+        dir = "row",
+      },
+
+      description = {
+        width = "40%",
+      },
+    },
+  },
 }, {
   ui = {
     border = "rounded",
