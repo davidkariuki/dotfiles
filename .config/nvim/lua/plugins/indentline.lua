67f0ec23adc0fe1c0a94a1e09c15c5b54f44cd1e
David Kariukí <davidkariuki@users.noreply.github.com>
2023-10-21 23:23:05 +0100

    Update indentline hooks

--- .config/nvim/lua/plugins/indentline.lua
+++ .config/nvim/lua/plugins/indentline.lua
@@ -3,6 +3,11 @@ return {
   main = "ibl",
   opts = {},
   config = function()
+    local hooks = require("ibl.hooks")
+
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+
     require("ibl").setup({
       indent = {
         char = "▏",
