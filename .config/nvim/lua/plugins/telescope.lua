3e252f4833a110034fd5133b3cb9d877b1891dc0
David Kariukí <davidkariuki@users.noreply.github.com>
2023-10-21 04:26:56 +0100

    Fix telescope search

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,41 +3,32 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-ui-select.nvim",
-    "kevinhwang91/nvim-bqf",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "DaikyXendo/nvim-web-devicons",
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
     telescope.setup({
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown({}),
-        },
-      },
       defaults = {
-        file_ignore_patterns = { "node_modules", "vendor", "dist", "build", "dotbot" },
-        path_display = { "truncate " },
-        prompt_prefix = " 🔍 ",
-        generic_sorter = require("telescope.sorters").get_fzy_sorter,
-        file_sorter = require("telescope.sorters").get_fzy_sorter,
-        layout_strategy = "flex",
-        use_less = true,
-        layout_config = {
-          horizontal = {
-            mirror = false,
-            preview_width = 0.6,
-          },
-          vertical = {
-            mirror = false,
-          },
+        vimgrep_arguments = {
+          "rg",
+          "--hidden",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--glob",
+          "!**/.git/*",
         },
+        path_display = { "truncate " },
+        file_ignore_patterns = { "node_modules", "vendor", "dist", "build", "dotbot" },
         mappings = {
           i = {
-            ["<esc>"] = actions.close,
+            ["<Esc>"] = actions.close,
             ["<C-k>"] = actions.move_selection_previous,
             ["<C-j>"] = actions.move_selection_next,
           },
@@ -46,21 +37,24 @@ return {
           },
         },
       },
+      pickers = {
+        find_files = {
+          find_command = {
+            "rg",
+            "--files",
+            "--hidden",
+            "--glob",
+            "!**/.git/*",
+          },
+        },
+      },
     })
 
     telescope.load_extension("fzf")
 
-    -- set keymaps
-    local map = vim.keymap.set
-
-    map(
-      "n",
-      "<C-p>",
-      "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
-      { noremap = true, silent = true }
-    )
-    map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {})
-    map("n", "<leader>g", "<cmd>Telescope live_grep<cr>", {})
-    map("n", "<leader>b", "<cmd>Telescope buffers<cr>", {})
+    vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", {})
+    vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {})
+    vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<cr>", {})
+    vim.keymap.set("n", "<leader>fs", "<cmd>Telescope grep_string<cr>", {})
   end,
 }
