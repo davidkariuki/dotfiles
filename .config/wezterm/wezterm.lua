a72c057ef410232233c2865dd35338faf1f0d83f
David Kariukí <davidkariuki@users.noreply.github.com>
2023-11-09 01:34:47 +0000

    goodbye kitty

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,12 @@
 local wezterm = require("wezterm")
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+custom.background = "#181825"
 
 return {
 	font = wezterm.font("MonoLisa Nerd Font"),
 	font_size = 9.5,
 	window_decorations = "NONE",
-	window_background_opacity = 0.98,
+	window_background_opacity = 0.95,
 	window_padding = {
 		left = 20,
 		right = 20,
@@ -13,18 +15,8 @@ return {
 	},
 	hide_tab_bar_if_only_one_tab = true,
 	force_reverse_video_cursor = true,
-	colors = {
-		foreground = "#dcd7ba",
-		background = "#16161D",
-		cursor_bg = "#c8c093",
-		cursor_fg = "#c8c093",
-		cursor_border = "#c8c093",
-		selection_fg = "#c8c093",
-		selection_bg = "#2d4f67",
-		scrollbar_thumb = "#16161d",
-		split = "#16161d",
-		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+	color_schemes = {
+		["Catppuccino Mocha"] = custom,
 	},
+	color_scheme = "Catppuccino Mocha",
 }
