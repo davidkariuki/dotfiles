cdf6f3b033b2cefc3048be58e54a477a4f131ec0
David Kariukí <davidkariuki@users.noreply.github.com>
2023-11-16 06:16:04 +0000

    Change floatterm border

--- .config/nvim/lua/plugins/floaterm.lua
+++ .config/nvim/lua/plugins/floaterm.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     vim.g.floaterm_width = 0.9
     vim.g.floaterm_height = 0.8
-    vim.cmd("hi FloatermBorder guifg=orange")
+    vim.cmd("hi FloatermBorder guifg=#b4befe")
     vim.keymap.set("n", "<A-t>", "<cmd>FloatermToggle<CR>", { noremap = true, silent = true })
     vim.keymap.set("t", "<A-t>", "<C-\\><C-n><cmd>FloatermToggle<CR>", { noremap = true, silent = true })
     vim.keymap.set("n", "<A-g>", "<cmd>FloatermNew lazygit<CR>", { noremap = true, silent = true })
