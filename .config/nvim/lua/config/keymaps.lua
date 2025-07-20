4f6690523f3cea47a2a17eb14340e64bae2b382a
David Kariukí <davidkariuki@users.noreply.github.com>
2025-07-20 21:17:44 +0100

    Update keymaps and plugin settings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,11 @@ local map = vim.keymap.set
 
 map("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Exit editing mode" })
 map("n", "<Leader>w", "<cmd>up<CR>", { noremap = true, desc = "Update file" })
+map("n", "<Leader>w", "<cmd>up<CR>", { noremap = true, desc = "Update file" })
+map(
+  "n",
+  "<leader><space>",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+map("n", "<C-p>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })
